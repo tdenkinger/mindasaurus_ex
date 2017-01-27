78f988f5398b508f6904b35c42871d8063c55ca1
Troy Denkinger <tdenkinger@gmail.com>
2017-01-27 08:26:30 -0500

    Clean up the test by using an alias.

--- apps/mindasaurus/test/minder_test.exs
+++ apps/mindasaurus/test/minder_test.exs
@@ -1,26 +1,26 @@
 defmodule Mindasaurus.MinderTest do
   use ExUnit.Case, async: true
-  doctest Mindasaurus.Minder
+  alias Mindasaurus.Minder
 
   setup do
-    {:ok, minder} = Mindasaurus.Minder.start_link
+    {:ok, minder} = Minder.start_link
     {:ok, minder: minder}
   end
 
   test "accepts a reminder", %{minder: minder} do
-    assert Mindasaurus.Minder.create(minder, :uuid12312, "buy coffee") == :ok
-    assert Mindasaurus.Minder.create(minder, :uuid12312, "eat food") == :ok
+    assert Minder.create(minder, :uuid12312, "buy coffee") == :ok
+    assert Minder.create(minder, :uuid12312, "eat food") == :ok
   end
 
   test "returns an empty list of reminders for a non-existent uuid", %{minder: minder} do
-    assert Mindasaurus.Minder.get(minder, :uuid12312) == []
+    assert Minder.get(minder, :uuid12312) == []
   end
 
   test "returns all reminders for a uuid that has them", %{minder: minder} do
-    Mindasaurus.Minder.create(minder, :uuid12312, "buy coffee")
-    Mindasaurus.Minder.create(minder, :uuid12312, "walk dog")
+    Minder.create(minder, :uuid12312, "buy coffee")
+    Minder.create(minder, :uuid12312, "walk dog")
 
-    reminders = Mindasaurus.Minder.get(minder, :uuid12312)
+    reminders = Minder.get(minder, :uuid12312)
     assert Enum.member?(reminders, "buy coffee")
     assert Enum.member?(reminders, "walk dog")
     refute Enum.member?(reminders, "don't exist")
