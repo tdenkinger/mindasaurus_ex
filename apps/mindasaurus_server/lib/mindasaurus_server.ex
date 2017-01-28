2b87ebb5ab275cb7c0b0d33517eed00e44d4760b
Troy Denkinger <tdenkinger@gmail.com>
2017-01-28 09:15:30 -0500

    Add supervision to the main genserver

---
+++ apps/mindasaurus_server/lib/mindasaurus_server.ex
@@ -0,0 +1,18 @@
+defmodule Mindasaurus do
+  @moduledoc """
+  Documentation for Mindasaurus.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mindasaurus.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/mindasaurus_server/test/mindasaurus_server_test.exs
@@ -0,0 +1,8 @@
+defmodule MindasaurusTest do
+  use ExUnit.Case
+  doctest Mindasaurus
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
