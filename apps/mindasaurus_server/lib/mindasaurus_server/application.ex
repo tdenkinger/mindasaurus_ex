90d7b4cb8db88c904824225fcb11cbcf48192462
Troy Denkinger <tdenkinger@gmail.com>
2017-01-28 10:27:14 -0500

    Setup supervision of the minder app

--- apps/mindasaurus_server/lib/mindasaurus_server/application.ex
+++ apps/mindasaurus_server/lib/mindasaurus_server/application.ex
@@ -11,7 +11,7 @@ defmodule Mindasaurus.Application do
     # Define workers and child supervisors to be supervised
     children = [
       # Starts a worker by calling: Mindasaurus.Worker.start_link(arg1, arg2, arg3)
-      # worker(Mindasaurus.Worker, [arg1, arg2, arg3]),
+      worker(Mindasaurus.Minder, [MinderMain]),
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
