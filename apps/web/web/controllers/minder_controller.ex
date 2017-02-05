e24a72907d9d6e3d189941e631200cdc692a4bca
Troy Denkinger <tdenkinger@gmail.com>
2017-02-05 15:03:30 -0500

    Update the web to use the correct domain project.

--- apps/web/web/controllers/minder_controller.ex
+++ apps/web/web/controllers/minder_controller.ex
@@ -2,12 +2,12 @@ defmodule Web.MinderController do
   use Web.Web, :controller
 
   def show(conn, %{"id" => id}) do
-    reminders = Mindasaurus.Minder.get(MinderMain, build_uuid(id))
+    reminders = Domain.Minder.get(MinderMain, build_uuid(id))
     render conn, "show.json", reminders: reminders
   end
 
   def create(conn, %{"id" => id, "reminder" => reminder}) do
-    status = Mindasaurus.Minder.create(MinderMain, build_uuid(id), reminder)
+    status = Domain.Minder.create(MinderMain, build_uuid(id), reminder)
     render conn, "create.json", status: status
   end
 
