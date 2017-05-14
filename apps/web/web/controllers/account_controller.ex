186c4a9dc34cce6cbd83d7d885c39b6dcea20e90
Troy Denkinger <tdenkinger@gmail.com>
2017-05-14 13:46:29 -0400

    Change the controllers to use the correct underlying app gateways.

--- apps/web/web/controllers/account_controller.ex
+++ apps/web/web/controllers/account_controller.ex
@@ -2,12 +2,12 @@ defmodule Web.AccountController do
   use Web.Web, :controller
 
   def create(conn, %{"username" => username, "email" => email, "password" => password}) do
-    {:ok, account} = Accounts.Handler.create(AccountHandler, username, email, password)
+    {:ok, account} = Accounts.Gateway.create(Gateway, username, email, password)
     render conn, "create.json", account
   end
 
   def show(conn, %{"username" => username, "password" => password}) do
-    case Accounts.Handler.login(AccountHandler, username, password) do
+    case Accounts.Gateway.login(Gateway, username, password) do
       {:ok, account}      -> render conn, "show.json", account
       {:error, "no user"} -> json(conn, %{error: "401"})
     end
--- apps/web/web/controllers/minder_controller.ex
+++ apps/web/web/controllers/minder_controller.ex
@@ -2,17 +2,17 @@ defmodule Web.MinderController do
   use Web.Web, :controller
 
   def show(conn, %{"id" => access_token}) do
-    reminders = Reminders.Minder.get(MinderMain, access_token)
+    reminders = Reminders.Gateway.get(Gateway, access_token)
     render conn, "show.json", reminders: reminders
   end
 
   def create(conn, %{"id" => access_token, "reminder" => reminder}) do
-    status = Reminders.Minder.create(MinderMain, access_token, reminder)
+    status = Reminders.Gateway.create(Gateway, access_token, reminder)
     render conn, "create.json", status: status
   end
 
   def delete(conn, %{"id" => reminder_id, "access_token" => access_token}) do
-    case Reminders.Minder.delete(MinderMain, access_token, reminder_id) do
+    case Reminders.Gateway.delete(Gateway, access_token, reminder_id) do
       {:ok, reminder} ->
         render conn, "delete.json", status: :ok, message: reminder.reminder
       {:error, error} ->
